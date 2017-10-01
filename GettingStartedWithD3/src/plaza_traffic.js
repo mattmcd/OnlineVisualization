fa9b934de88b17973aeb9b1acd11ec2642871812
mattmcd <matt@matt-mcdonnell.com>
2017-10-01 11:36:32 +0100

    Plaza traffic example with style and labels

--- GettingStartedWithD3/src/plaza_traffic.js
+++ GettingStartedWithD3/src/plaza_traffic.js
@@ -2,24 +2,54 @@ var pt_vis;
 
 pt_vis = (function () {
     function draw_div(data) {
-        d3.select('body')
+        var sel = d3.select('body')
             .append('div')
             .attr('class', 'chart')
             .selectAll('bar')
             .data(data['cash'])
-            .enter()
-            .append('div')
+            .enter();
+
+        draw_div_bar(sel);
+    }
+
+    function label_div(sel){
+        console.log('labeling div');
+        sel.append('div')
+            .attr('class', 'label')
+            .text(function (d) {
+                return d.name
+            })
+    }
+
+    function draw_div_bar(sel){
+        console.log('drawing div bar');
+        sel.append('div')
             .attr('class', 'bar')
             .style('width', function (d) {
                 return d.count/100 + 'px';
             })
-            .style('outline', '1pt solid black')
+            //.style('outline', '1pt solid black')
             .text(function (d) {
                 return Math.round(d.count)
             })
     }
 
+    function draw_labeled_div(data) {
+        d3.select('body')
+            .append('div')
+            .attr('class', 'chart')
+            .selectAll('div.line')
+            .data(data['cash'])
+            .enter()
+            .append('div')
+            .attr('class', 'line');
+
+        label_div(d3.selectAll('div.line'));
+        draw_div_bar(d3.selectAll('div.line'));
+    }
+
     return {
-        'draw_div': draw_div
+        'draw_div': draw_div,
+        'draw_labeled_div': draw_labeled_div
     };
 })();
